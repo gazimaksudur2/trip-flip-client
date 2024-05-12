4c4e4d3487c810662d6c80179f5c0d96846ed5c7
GaziMaksudur <gazimaksudur2@gmail.com>
2024-05-12 20:25:33 +0600

    Contact Page COmponent imported from Mereki

--- src/components/myBookings/BookingTable.jsx
+++ src/components/myBookings/BookingTable.jsx
@@ -5,7 +5,7 @@ const BookingTable = () => {
         <div className='my-3'>
             <section className="container px-4 mx-auto">
                 <div className="flex items-center gap-x-3">
-                    <h2 className="text-lg font-medium text-gray-800 ">Team members</h2>
+                    <h2 className="text-lg font-medium text-gray-800 ">Your Activity</h2>
 
                     <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full -400">4 Bookings</span>
                 </div>
@@ -19,26 +19,19 @@ const BookingTable = () => {
                                         <tr>
                                             <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 -400">
                                                 <div className="flex items-center gap-x-3">
-                                                    <input type="checkbox" className="text-blue-500 border-gray-300 rounded"/>
-                                                        <span>Name</span>
+                                                        <span>Booked Room</span>
                                                 </div>
                                             </th>
 
                                             <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 -400">
                                                 <button className="flex items-center gap-x-2">
-                                                    <span>Status</span>
-
-                                                    <svg className="h-3" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z" fill="currentColor" stroke="currentColor" strokeWidth="0.1" />
-                                                        <path d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z" fill="currentColor" stroke="currentColor" strokeWidth="0.1" />
-                                                        <path d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z" fill="currentColor" stroke="currentColor" strokeWidth="0.3" />
-                                                    </svg>
+                                                    <span>Category</span>
                                                 </button>
                                             </th>
 
                                             <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 -400">
                                                 <button className="flex items-center gap-x-2">
-                                                    <span>Role</span>
+                                                    <span>Booked At</span>
 
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-4 h-4">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
@@ -46,9 +39,9 @@ const BookingTable = () => {
                                                 </button>
                                             </th>
 
-                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 -400">Email address</th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 -400">Check In Date</th>
 
-                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 -400">Teams</th>
+                                            <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 -400">Paid Amount</th>
 
                                             <th scope="col" className="relative py-3.5 px-4">
                                                 <span className="sr-only">Edit</span>
@@ -88,6 +81,19 @@ const BookingTable = () => {
                                             </td>
                                             <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                 <div className="flex items-center gap-x-6">
+                                                    
+                                                    
+                                                    {/* Make a review From here with using icon as button  */}
+
+
+
+
+                                                    <button className="text-gray-500 transition-colors duration-200 -300 hover:text-red-500 focus:outline-none">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                        </svg>
+                                                    </button>
+
                                                     <button className="text-gray-500 transition-colors duration-200 -300 hover:text-red-500 focus:outline-none">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import SwiperSlider from '../components/Rooms/SwiperSlider';
-import MySlider from '../components/contacts_comps/MySlider';
+import VisitStores from '../components/contacts_comps/VisitStores';
+import GetInTouch from '../components/contacts_comps/GetInTouch';
+// import SwiperSlider from '../components/Rooms/SwiperSlider';
+// import MySlider from '../components/contacts_comps/MySlider';
 
 const Contacts = () => {
     return (
         <div>
             <h2 className='text-2xl font-semibold font-source'>This is from Contacts</h2>
             <div className='py-10'>
-                <SwiperSlider />
+                {/* <SwiperSlider /> */}
                 {/* <MySlider/> */}
+                {/* <VisitStores/> */}
+                {/* <GetInTouch/> */}
             </div>
         </div>
     );
